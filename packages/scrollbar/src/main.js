0e5b1e94aa9fce5064893c10ca82e24e93d9fc7e
callmewhy <whywanghai@gmail.com>
2017-04-07 13:37:01 +0800

    fix error in scrollbar

--- packages/scrollbar/src/main.js
+++ packages/scrollbar/src/main.js
@@ -119,8 +119,8 @@ export default {
     !this.noresize && addResizeListener(this.$refs.resize, this.update);
   },
 
-  destroyed() {
+  beforeDestroy() {
     if (this.native) return;
-    !this.noresize && removeResizeListener(this.$refs.resize, this.update);
+    !this.noresize && this.$refs.resize && removeResizeListener(this.$refs.resize, this.update);
   }
 };
